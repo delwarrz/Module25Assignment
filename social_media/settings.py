4ed171e74318f2e637531f4a4d63311024360b1a
Delwar Hossain <delwar.ewu@gmail.com>
2025-02-23 18:46:43 +0600

    adding login and register route

--- social_media/settings.py
+++ social_media/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts'
+    'posts',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'login'  # optional: redirect after login
