b671c9bdd73bf8701328973f9c880580d2d37bc5
Delwar Hossain <delwar.ewu@gmail.com>
2025-02-23 18:34:23 +0600

    Initial commit

---
+++ posts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User  # added import
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Post by {self.author.username} at {self.created_at}"
